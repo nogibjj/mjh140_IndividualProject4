837de1d29ec293b5e4275b314e6b2974ff5358d8
Matthew Holden <matthew.holden86@gmail.com>
2023-12-10 12:32:17 -0500

    flask app complete

--- Makefile
+++ Makefile
@@ -1,18 +1,16 @@
+venv:
+	python3 -m venv myenv
+		source myenv/bin/activate
+
 install:
 	pip install --upgrade pip &&\
 		pip install -r requirements.txt
 
-test:
-	python -m pytest -vv --cov=main --cov=mylib tests/
-
 format:	
-	black mylib/*.py tests/*.py src/*.py
+	black *.py
 
 lint:
-	#disable comment to test speed
-	#pylint --disable=R,C --ignore-patterns=tests/*.py mylib/*.py
-	#ruff linting is 10-100X faster than pylint
-	ruff check mylib/*.py
+	ruff check *.py
 
 container-lint:
 	docker run --rm -i hadolint/hadolint < Dockerfile
@@ -21,5 +19,28 @@ refactor: format lint
 
 deploy:
 	#deploy goes here
+		
+all: venv install format lint refactor deploy
+
+######################################
+
+venv:
+	python3 -m venv myenv
+		source myenv/bin/activate
+
+install:
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+
+format:	
+	black *.py 
+
+lint:
+	ruff check *.py 
+
+container-lint:
+	docker run --rm -i hadolint/hadolint < Dockerfile
+
+refactor: format lint
 		
 all: install lint test format deploy
---
+++ flask_app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from transformers import pipeline, set_seed
+import textwrap
+
+app = Flask(__name__)
+
+# Set up the text generation pipeline with GPT-2 model
+summarizer = pipeline("summarization", model = 'sshleifer/distilbart-cnn-12-6')
+set_seed(42)  # Optional: Set a seed for reproducibility
+
+# text wrapping function
+def wrap(x):
+  return textwrap.fill(x, replace_whitespace = False, fix_sentence_endings = True)
+
+@app.route("/")
+def index():
+    return render_template("traits.html")
+
+@app.route("/summarize", methods=["POST"])
+def summarize():
+    if request.method == "POST":
+
+        input_text = str(request.form["prompt"])
+
+        doc = wrap(input_text)
+        
+        # Summarize text using Hugging Face summarization transformer
+        results = summarizer(doc.split('\n',1)[1])
+
+        summary = results[0]['summary_text']
+
+        print(summary)
+        
+        return render_template(
+            "summary.html", summarized_text = summary
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
